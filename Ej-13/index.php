75bc267c797992662e101529225d391535756a0a
NaN <mauriciodominguez9876543@gmail.com>
2024-09-07 12:00:12 -0300

    end 14ej

--- Ej-13/index.php
+++ Ej-13/index.php
@@ -15,12 +15,47 @@ require_once ("../Ej-8 Punto de partida/header.php");
 ?>
 
 <section class="w3-center">
-    <form action="matrix.php" method="post">
-        <h1>quiere pedir</h1> <!--Ya que es una matriz cuadrada los valores deben ser iguals -->
-        <label for="cantidad"><input type="number" name="cantidad" id="cantidad"></label>
+    <form action="index.php" method="post">
+
+        <h1>Menu no saludable</h1>
+
+        <label for="entrada">¿quiere pedir entrada?</label>
+        <input type="checkbox" name="entrada" id="entrada"><br>
+
+        <label for="plato_principal">¿quiere pedir plato_principal?</label>
+        <input type="checkbox" name="plato_principal" id="plato_principal"><br>
+
+        <label for="acompanamiento">¿quiere pedir acompañamiento?</label>
+        <input type="checkbox" name="acompanamiento" id="acompanamiento"><br>
+
+        <label for="postre">¿quiere pedir postre?</label>
+        <input type="checkbox" name="postre" id="postre"><br>
+
         <label for="enviar"><input type="submit" value="enviar"></label>
     </form>
 </section>
 
+<section class="w3-center">
+    <?php
+    mostrarDetallesDelMenu();
+    ?>
+</section>
+
 </body>
 </html>
+
+<?php
+
+function mostrarDetallesDelMenu()
+{
+    $archivo = parse_ini_file("menu.ini", true);
+    //$_POST["acompanamiento"]="acompanamiento";
+    //echo $archivo[$_POST["acompanamiento"]]["opcion1"];
+    //var_dump($archivo);
+    //var_dump($_POST);
+
+    foreach ($_POST as $comida => $valor) {
+        echo "<br>" . "su menu con: " . $archivo[$comida]["opcion1"] . " esta en camino";
+    }
+}
+?>
